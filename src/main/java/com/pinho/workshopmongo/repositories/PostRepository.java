411b5d69a089470912313322996cca0dae664256
MVBPinho <marcos.vb.pinho@live.com>
2023-07-08 13:27:41 -0300

    Consulta simples com query methods

--- src/main/java/com/pinho/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/pinho/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.pinho.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.pinho.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
