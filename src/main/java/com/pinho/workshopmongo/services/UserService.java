8e778cfa5684e572e84632570217c42435b2a5eb
MVBPinho <marcos.vb.pinho@live.com>
2023-07-07 21:50:14 -0300

    Inserção de usuário com POST

--- src/main/java/com/pinho/workshopmongo/services/UserService.java
+++ src/main/java/com/pinho/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pinho.workshopmongo.domain.User;
+import com.pinho.workshopmongo.dto.UserDTO;
 import com.pinho.workshopmongo.repositories.UserRepository;
 import com.pinho.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,7 +25,12 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
-
-
-
