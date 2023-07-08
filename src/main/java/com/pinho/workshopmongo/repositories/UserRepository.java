85a0f25ba314f722096037d81d4971247a3b3de6
MVBPinho <marcos.vb.pinho@live.com>
2023-07-07 21:08:42 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/pinho/workshopmongo/repositories/UserRepository.java
+++ src/main/java/com/pinho/workshopmongo/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.pinho.workshopmongo.domain.User;
 
-public interface UserRepository extends MongoRepository<User, Long>{
+public interface UserRepository extends MongoRepository<User, String>{
 
 }
